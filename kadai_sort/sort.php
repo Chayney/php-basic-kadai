fd92238bb6122b746c4b968547a330a03e0cdb9e
Chayney <158685403+Chayney@users.noreply.github.com>
2024-03-02 17:16:57 +0900

    kadai_sortフォルダをアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,22 @@
+<?php
+
+$nums = [15, 4, 18, 23, 10 ];
+
+function sort_2way($array,$order) {
+    if($order === TRUE) {
+        echo '昇順にソートします。' . '<br>';
+        sort($array);
+    } else {
+        echo '降順にソートします。' . '<br>';
+        rsort($array);
+    }
+    foreach($array as $arr) {
+        echo $arr . '<br>';
+    }
+}
+
+sort_2way($nums,TRUE);
+
+sort_2way($nums,FALSE);
+
+?>
